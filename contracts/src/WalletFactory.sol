93ceed8b9d9f7c7a671862d98f50a1de2d8d8c37
Danny Pr <47109543+dannpr@users.noreply.github.com>
2023-12-04 23:33:42 +0100

    add contracts codes

--- contracts/src/WalletFactory.sol
+++ contracts/src/WalletFactory.sol
@@ -5,10 +5,7 @@ import {IEntryPoint} from "account-abstraction/interfaces/IEntryPoint.sol";
 import {Wallet} from "./Wallet.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import {Create2} from "@openzeppelin/contracts/utils/Create2.sol";
-
-/* 
-import {Zapper} from "./../Zapper.sol";
-*/
+import {Zapper} from "./Zapper.sol";
 
 contract WalletFactory {
     Wallet public immutable walletImplementation;
