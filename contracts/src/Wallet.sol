d2053875e82505d860d27dc3fa481a0e44fbc7aa
Danny Pr <47109543+dannpr@users.noreply.github.com>
2023-12-05 13:35:40 +0100

    working on opzep import

--- contracts/src/Wallet.sol
+++ contracts/src/Wallet.sol
@@ -13,10 +13,7 @@ import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.s
 import {UUPSUpgradeable} from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 import {TokenCallbackHandler} from "account-abstraction/samples/callback/TokenCallbackHandler.sol";
 
-/* 
-import {Zapper} from "./../Zapper.sol";
-import {BAYCPaymaster} from "./BAYCPaymaster.sol"; 
-*/
+import {Zapper} from "./Zapper.sol";
 
 contract Wallet is
     BaseAccount,
@@ -25,7 +22,7 @@ contract Wallet is
     TokenCallbackHandler
 {
     using ECDSA for bytes32;
-    address[] public owners;
+    address public owner;
 
     // immutable to keep track of them
     address public immutable walletFactory;
@@ -39,7 +36,8 @@ contract Wallet is
         _;
     }
 
-    event WalletInitialized(IEntryPoint indexed entryPoint, address[] owners);
+    event WalletInitialized(IEntryPoint indexed entryPoint, address owner);
+
     modifier _requireFromEntryPointOrFactory() {
         require(
             msg.sender == address(_entryPoint) || msg.sender == walletFactory,
@@ -54,14 +52,15 @@ contract Wallet is
     }
 
     //////////////////////INITIALIZER//////////////////////////
-    function initialize(address[] memory initialOwners) public initializer {
-        _initialize(initialOwners);
+    function initialize(address memory initOwner) public initializer {
+        _initialize(initOwner);
     }
 
-    function _initialize(address[] memory initialOwners) internal {
-        require(initialOwners.length > 0, "no owners");
-        owners = initialOwners;
-        emit WalletInitialized(_entryPoint, initialOwners);
+    function _initialize(address memory initOwner) internal {
+        //require(initialOwners.length > 0, "no owners");
+        // verify that the owner is valid through a secp256r1 signature
+        owner = initOwner;
+        emit WalletInitialized(_entryPoint, initOwner);
     }
 
     //////////////////////UPGRADE//////////////////////////
@@ -74,6 +73,8 @@ contract Wallet is
         return _entryPoint;
     }
 
+// create a new way to validate the signature
+// Passkey, digital new signature, etc.
     function _validateSignature(
         UserOperation calldata userOp, // UserOperation data structure passed as input
         bytes32 userOpHash // Hash of the UserOperation without the signatures
@@ -81,17 +82,9 @@ contract Wallet is
         // Convert the userOpHash to an Ethereum Signed Message Hash
         bytes32 hash = userOpHash.toEthSignedMessageHash();
 
-        // Decode the signatures from the userOp and store them in a bytes array in memory
-        bytes[] memory signatures = abi.decode(userOp.signature, (bytes[]));
+        if (owner != hash.recover(userOp.signature))
+            return SIG_VALIDATION_FAILED;
 
-        // Loop through all the owners of the wallet
-        for (uint256 i = 0; i < owners.length; i++) {
-            // Recover the signer's address from each signature
-            // If the recovered address doesn't match the owner's address, return SIG_VALIDATION_FAILED
-            if (owners[i] != hash.recover(signatures[i])) {
-                return SIG_VALIDATION_FAILED;
-            }
-        }
         // If all signatures are valid (i.e., they all belong to the owners), return 0
         return 0;
     }
